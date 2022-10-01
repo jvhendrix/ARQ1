ee8d507af32db9fb78c734c07e67823ddff73894
jvhendrix <joaovitorsouza5000@gmail.com>
2022-09-30 23:20:31 -0300

    concatenate 2 strings (file extenstion in the end of file name)

--- Trabalho Intel/crypto.asm
+++ Trabalho Intel/crypto.asm
@@ -13,19 +13,28 @@ CR		equ		0dh
 LF		equ		0ah
 
 	.data
-FileNameSrc		db		256 dup (?)		; Nome do arquivo a ser lido
+
+file_name_src	db		256 dup (?)		; Nome do arquivo a ser lido
 FileNameDst		db		256 dup (?)		; Nome do arquivo a ser escrito
 FileHandleSrc	dw		0				; Handler do arquivo origem
 FileHandleDst	dw		0				; Handler do arquivo destino
 FileBuffer		db		10 dup (?)		; Buffer de leitura/escrita do arquivo
 
-MsgPedeArquivoSrc	db	"Nome do arquivo origem: ", 0
-MsgPedeArquivoDst	db	"Nome do arquivo destino: ", 0
-MsgErroOpenFile		db	"Erro na abertura do arquivo.", CR, LF, 0
-MsgErroCreateFile	db	"Erro na criacao do arquivo.", CR, LF, 0
-MsgErroReadFile		db	"Erro na leitura do arquivo.", CR, LF, 0
-MsgErroWriteFile	db	"Erro na escrita do arquivo.", CR, LF, 0
-MsgCRLF				db	CR, LF, 0
+msg_pede_arquivo_src	db	"Nome do arquivo origem: ", 0
+MsgPedeArquivoDst		db	"Nome do arquivo destino: ", 0
+MsgErroOpenFile			db	"Erro na abertura do arquivo.", CR, LF, 0
+MsgErroCreateFile		db	"Erro na criacao do arquivo.", CR, LF, 0
+MsgErroReadFile			db	"Erro na leitura do arquivo.", CR, LF, 0
+MsgErroWriteFile		db	"Erro na escrita do arquivo.", CR, LF, 0
+
+file_extenstion_txt 	db	".txt", 0	; Extensão .txt
+file_extenstion_krp		db	".krp", 0	; Extensão .krp
+msg_crlf				db	CR, LF, 0	; Caracteres '\r' e '\n'
+
+
+
+
+ajuda				db	"Esse eh teste", CR, LF, 0
 
 MAXSTRING	equ		200
 String	db		MAXSTRING dup (?)		; Usado na funcao gets
@@ -33,250 +42,111 @@ String	db		MAXSTRING dup (?)		; Usado na funcao gets
 	.code
 	.startup
 
-	;GetFileNameSrc();	// Pega o nome do arquivo de origem -> FileNameSrc
-	call	GetFileNameSrc
-
-	;if (fopen(FileNameSrc)) {
-	;	printf("Erro na abertura do arquivo.\r\n")
-	;	exit(1)
-	;}
-	;FileHandleSrc = BX
-	lea		dx,FileNameSrc
-	call	fopen
-	mov		FileHandleSrc,bx
-	jnc		Continua1
-	lea		bx, MsgErroOpenFile
+	call	getFileName
+	lea		bx, file_name_src
 	call	printf_s
-	.exit	1
-Continua1:
 
-	;GetFileNameDst();	// Pega o nome do arquivo de origem -> FileNameDst
-	call	GetFileNameDst
-	
-	;if (fcreate(FileNameDst)) {
-	;	fclose(FileHandleSrc);
-	;	printf("Erro na criacao do arquivo.\r\n")
-	;	exit(1)
-	;}
-	;FileHandleDst = BX
-	lea		dx,FileNameDst
-	call	fcreate
-	mov		FileHandleDst,bx
-	jnc		Continua2
-	mov		bx,FileHandleSrc
-	call	fclose
-	lea		bx, MsgErroCreateFile
-	call	printf_s
-	.exit	1
-Continua2:
+	.exit 0
 
-	;do {
-	;	if ( (CF,DL,AX = getChar(FileHandleSrc)) ) {
-	;		printf("");
-	;		fclose(FileHandleSrc)
-	;		fclose(FileHandleDst)
-	;		exit(1)
-	;	}
-	mov		bx,FileHandleSrc
-	call	getChar
-	jnc		Continua3
-	lea		bx, MsgErroReadFile
-	call	printf_s
-	mov		bx,FileHandleSrc
-	call	fclose
-	mov		bx,FileHandleDst
-	call	fclose
-	.exit	1
-Continua3:
 
-	;	if (AX==0) break;
-	cmp		ax,0
-	jz		TerminouArquivo
-	
-	;	dl = toUpper(dl)
-	cmp		dl,'a'
-	jb		Continua4
-	cmp		dl,'z'
-	ja		Continua4
-	sub		dl,20h		
-Continua4:
-
-	;	if ( setChar(FileHandleDst, DL) == 0) continue;
-	mov		bx,FileHandleDst
-	call	setChar
-	jnc		Continua2
-
-	;	printf ("Erro na escrita....;)")
-	;	fclose(FileHandleSrc)
-	;	fclose(FileHandleDst)
-	;	exit(1)
-	lea		bx, MsgErroWriteFile
-	call	printf_s
-	mov		bx,FileHandleSrc		; Fecha arquivo origem
-	call	fclose
-	mov		bx,FileHandleDst		; Fecha arquivo destino
-	call	fclose
-	.exit	1
-	
-	;} while(1);
-		
-TerminouArquivo:
-	;fclose(FileHandleSrc)
-	;fclose(FileHandleDst)
-	;exit(0)
-	mov		bx,FileHandleSrc	; Fecha arquivo origem
-	call	fclose
-	mov		bx,FileHandleDst	; Fecha arquivo destino
-	call	fclose
-	.exit	0
-
-		
 ;--------------------------------------------------------------------
-;Funcao Pede o nome do arquivo de origem salva-o em FileNameSrc
+;getFileName
+;Funcao para ler o nome do arqvuio a ser lido
 ;--------------------------------------------------------------------
-GetFileNameSrc	proc	near
-	;printf("Nome do arquivo origem: ")
-	lea		bx, MsgPedeArquivoSrc
-	call	printf_s
-
-	;gets(FileNameSrc);
-	lea		bx, FileNameSrc
-	call	gets
+getFileName	proc	near
 	
-	;printf("\r\n")
-	lea		bx, MsgCRLF
-	call	printf_s
+	lea		bx, msg_pede_arquivo_src		; Coloca endereço da string em bx
+	call	printf_s						; Printa a string
+
+	lea		bx, file_name_src				; Coloca o endereço de file_name_src em bx
+	call	gets							; Coloca nome do arquivo em file_name_src
+
+	lea		bx, file_name_src				; Coloca o endereço de file_name_src em bx
+	lea		cx, file_extenstion_txt			; Coloca o endereço de file_extension_txt em cx
+	call 	putFileExtenstion				; Coloca extensão em file_name_src
+
+	lea		bx, msg_crlf					; Coloca o endereço de msg_crlf em bx
+	call	printf_s						; Printa a string ("\r\n") (nova linha, no começo da linha)
 	
 	ret
-GetFileNameSrc	endp
+
+getFileName	endp
 
 
 ;--------------------------------------------------------------------
-;Funcao Pede o nome do arquivo de destino salva-o em FileNameDst
-;--------------------------------------------------------------------
-GetFileNameDst	proc	near
-	;printf("Nome do arquivo destino: ");
-	lea		bx, MsgPedeArquivoDst
-	call	printf_s
-	
-	;gets(FileNameDst);
-	lea		bx, FileNameDst
-	call	gets
-	
-	;printf("\r\n")
-	lea		bx, MsgCRLF
-	call	printf_s
-	
-	ret
-GetFileNameDst	endp
-
-;--------------------------------------------------------------------
-;Fun��o	Abre o arquivo cujo nome est� no string apontado por DX
-;		boolean fopen(char *FileName -> DX)
-;Entra: DX -> ponteiro para o string com o nome do arquivo
-;Sai:   BX -> handle do arquivo
-;       CF -> 0, se OK
-;--------------------------------------------------------------------
-fopen	proc	near
-	mov		al,0
-	mov		ah,3dh
-	int		21h
-	mov		bx,ax
-	ret
-fopen	endp
-
-;--------------------------------------------------------------------
-;Fun��o Cria o arquivo cujo nome est� no string apontado por DX
-;		boolean fcreate(char *FileName -> DX)
-;Sai:   BX -> handle do arquivo
-;       CF -> 0, se OK
-;--------------------------------------------------------------------
-fcreate	proc	near
-	mov		cx,0
-	mov		ah,3ch
-	int		21h
-	mov		bx,ax
-	ret
-fcreate	endp
-
-;--------------------------------------------------------------------
-;Entra:	BX -> file handle
-;Sai:	CF -> "0" se OK
-;--------------------------------------------------------------------
-fclose	proc	near
-	mov		ah,3eh
-	int		21h
-	ret
-fclose	endp
-
-;--------------------------------------------------------------------
-;Fun��o	Le um caractere do arquivo identificado pelo HANLDE BX
-;		getChar(handle->BX)
-;Entra: BX -> file handle
-;Sai:   dl -> caractere
-;		AX -> numero de caracteres lidos
-;		CF -> "0" se leitura ok
-;--------------------------------------------------------------------
-getChar	proc	near
-	mov		ah,3fh
-	mov		cx,1
-	lea		dx,FileBuffer
-	int		21h
-	mov		dl,FileBuffer
-	ret
-getChar	endp
-		
-;--------------------------------------------------------------------
-;Entra: BX -> file handle
-;       dl -> caractere
-;Sai:   AX -> numero de caracteres escritos
-;		CF -> "0" se escrita ok
-;--------------------------------------------------------------------
-setChar	proc	near
-	mov		ah,40h
-	mov		cx,1
-	mov		FileBuffer,dl
-	lea		dx,FileBuffer
-	int		21h
-	ret
-setChar	endp	
-
-
+;gets
 ;
-;--------------------------------------------------------------------
-;Funcao Le um string do teclado e coloca no buffer apontado por BX
-;		gets(char *s -> bx)
+;	Função lê um string do teclado e coloca no buffer apontado por BX
+;
+;	bx = endereço da string que terá o nome do arquivo
 ;--------------------------------------------------------------------
 gets	proc	near
+	
 	push	bx
 
-	mov		ah,0ah						; L� uma linha do teclado
+	mov		ah,0ah							; Lê uma linha do teclado
 	lea		dx,String
 	mov		byte ptr String, MAXSTRING-4	; 2 caracteres no inicio e um eventual CR LF no final
 	int		21h
 
-	lea		si,String+2					; Copia do buffer de teclado para o FileName
+	lea		si,String+2						; Copia do buffer de teclado para o FileName
 	pop		di
 	mov		cl,String+1
 	mov		ch,0
-	mov		ax,ds						; Ajusta ES=DS para poder usar o MOVSB
+	mov		ax,ds							; Ajusta ES=DS para poder usar o MOVSB
 	mov		es,ax
 	rep 	movsb
 
-	mov		byte ptr es:[di],0			; Coloca marca de fim de string
+	mov		byte ptr es:[di], 0				; Coloca marca de fim de string
 	ret
+
 gets	endp
 
-;====================================================================
-; A partir daqui, est�o as fun��es j� desenvolvidas
-;	1) printf_s
-;====================================================================
+;--------------------------------------------------------------------
+;putFileExtension
+;	
+;	Essa função coloca a extensão em uma string
+;
+;	bx = endereço da string com nome do arquivo
+;	cx = endereço da string com a extensão a ser colocada
+;--------------------------------------------------------------------
+putFileExtenstion	proc	near	
 	
-;--------------------------------------------------------------------
-;Fun��o Escrever um string na tela
-;		printf_s(char *s -> BX)
-;--------------------------------------------------------------------
+	mov		ax,ds							; Ajusta ES=DS para poder usar o MOVSB
+	mov		es,ax							;
+
+	loop_putFileExtenstion:					; Loop para procurar o final da string de file_name_src
+		
+		cmp  	[bx], 0						; Verifica se a string está no final
+		je		end_loop_putFileExtenstion	; Se estiver, sair do loop
+		inc		bx							; Caso contrário, incrementar endereço da string
+		jmp		loop_putFileExtenstion		; Voltar para o loop
+
+	end_loop_putFileExtenstion:	
+	
+	mov		di, bx							; Coloca o endereço final da string em di
+	mov		si, cx							; Coloca o endereço da string da extensão do arquivo em si
+	rep		movsb							; Copia a extensão para a string do nome do arquivo
+	
+	mov		byte ptr es:[di], 0				; Coloca a marca de fim da string
+	ret
+
+putFileExtenstion	endp
+
+
+
+
+
+;====================================================================
+;printf_s		
+;	
+;	Essa função printa uma string na tela
+;	
+;	bx = endereço da string
+;====================================================================
+
 printf_s	proc	near
+	
 	mov		dl,[bx]
 	cmp		dl,0
 	je		ps_1
@@ -293,14 +163,4 @@ ps_1:
 	ret
 printf_s	endp
 
-;--------------------------------------------------------------------
-		end
-;--------------------------------------------------------------------
-
-
-	
-
-
-
-
-
+	end
